a83c7e695b453f514f900a9ccd564cf2ba3783fb
eduardo <eduardoo.andrees@gmail.com>
2015-10-12 22:49:08 -0300

    original

---
+++ domino/src/domino/Domino.java
@@ -0,0 +1,16 @@
+package domino;
+
+import java.util.Scanner;
+
+public class Domino {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		int a = entrada.nextInt();
+		
+		int b = entrada.nextInt();
+		
+		System.out.println((a*b)/2);
+
+   }
+}
